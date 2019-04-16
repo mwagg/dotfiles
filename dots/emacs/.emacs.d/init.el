58b3fe6600c17d85f6cc843dddda5b797282d4d2
mwagg <michael@guerillatactics.co.uk>
2019-04-16 08:39:11 +0100

    Disable auto update of packages

--- dots/emacs/.emacs.d/init.el
+++ dots/emacs/.emacs.d/init.el
@@ -599,8 +599,7 @@
   :ensure t
   :config
   (setq auto-package-update-delete-old-versions t
-        auto-package-update-interval 4)
-  (auto-package-update-maybe))
+        auto-package-update-interval 4))
 
 (use-package fish-mode)
 
