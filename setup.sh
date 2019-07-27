995168e6c0e3758f7a30e2dd0759d7357da0806d
mwagg <michael@guerillatactics.co.uk>
2019-07-27 18:52:22 +0100

    Always start tmux when opening terminal

--- setup.sh
+++ setup.sh
@@ -106,7 +106,7 @@ pacman_install gnome-control-center
 pacman_install gnome-backgrounds
 pacman_install gnome-tweaks
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal -e tmux'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>Return'
 
 # vim
