d1d76f8eeccf670252032c2f521d8761e1ea1457
mwagg <michael@guerillatactics.co.uk>
2019-08-06 18:07:38 +0100

    Add keybindings to run python tests

--- dots/emacs/.emacs.d/lisp/init-python.el
+++ dots/emacs/.emacs.d/lisp/init-python.el
@@ -12,7 +12,11 @@
   (use-package highlight-indentation)
   (use-package company-jedi
     :config
-    (add-hook 'python-mode-hook (lambda () (add-to-list 'company-backends 'company-jedi)))))
+    (add-hook 'python-mode-hook (lambda () (add-to-list 'company-backends 'company-jedi))))
+  :general
+  (local-leader-def 'python-mode-map
+    "t" '(:ignore t :which0key "test")
+    "tt" 'elpy-test))
 
 (provide 'init-python)
 
