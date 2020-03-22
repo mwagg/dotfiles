241fbfa2898be406486362e087fdbe41ba790f9b
mwagg <michael@guerillatactics.co.uk>
2020-03-22 16:42:55 +0000

    Fix partiton script logging

--- arch/partition.sh
+++ arch/partition.sh
@@ -9,13 +9,13 @@ fi
 
 PARTITION_PREFIX=${PARTITION_PREFIX:-""}
 
-log "Partitioning disk"
+echo "Partitioning disk"
 parted --script -a optimal /dev/$DRIVE \
        mklabel gpt \
        mkpart primary 1MiB 512MiB \
        set 1 esp on \
        -- mkpart primary 512Mib -1
 
-log "Formatting boot partition"
+echo "Formatting boot partition"
 BOOT_DEV="/dev/${DRIVE}${PARTITION_PREFIX}1"
 mkfs.vfat -F32 $BOOT_DEV
