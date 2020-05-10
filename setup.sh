7e1df8d61211f35fe2eb3cec7574d9627589956c
mwagg <michael@guerillatactics.co.uk>
2020-05-10 18:57:46 +0100

    compton -> picom

--- setup.sh
+++ setup.sh
@@ -266,8 +266,7 @@ pacman_install spectrwm
 pacman_install man-db
 pacman_install man-pages
 pacman_install nitrogen
-pacman_install compton
+pacman_install picom
 pacman_install xlockmore
-yay_install polybar-git
 
 sudo cp templates/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
