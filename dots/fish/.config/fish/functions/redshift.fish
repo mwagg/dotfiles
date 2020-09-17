50496bca96726c17a8dabadf603248c4a508e691
mwagg <michael@guerillatactics.co.uk>
2020-09-17 11:48:27 +0100

    Manage SSH connection for redshift

--- dots/fish/.config/fish/functions/redshift.fish
+++ dots/fish/.config/fish/functions/redshift.fish
@@ -1,4 +1,5 @@
 function redshift --no-scope-shadowing
+    ssh -f -o ExitOnForwardFailure=yes bastion sleep 10
     set -x PGPASSWORD (aws ssm get-parameter --name "/prod/redshift/password" --with-decryption --output text | awk '{print $6}')
     psql -h localhost -p 5439 -U app_redshift analytics
 end
