10e24f414e6c6c802c429d3b6687a0943f2a45b5
mwagg <michael@guerillatactics.co.uk>
2020-10-04 15:24:46 +0100

    Fix git-clean script

--- dots/fish/.config/fish/functions/git-clean.fish
+++ dots/fish/.config/fish/functions/git-clean.fish
@@ -3,14 +3,14 @@ function git-clean --no-scope-shadowing
     git remote prune origin
 
     echo "Deleting local branches which are already merged"
-    for branch in (git branch --merged master | grep -v master | grep -v "*")
+    for branch in (git branch --merged master | grep -v master | grep -v "*" | string trim)
         git branch -d $branch
     end
 
     echo "Removing local branches not present on origin"
-    for branch in (git branch | grep -v master | grep -v "*")
+    for branch in (git branch | grep -v master | grep -v "*" | string trim)
         git branch -r | grep $branch > /dev/null
-        if test $status == 1
+        if test $status = 1
             git branch -D $branch
         end
     end
