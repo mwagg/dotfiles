13e46e4d15fceeff0c82fb96d62e81b404f37448
mwagg <michael@guerillatactics.co.uk>
2020-10-14 10:29:50 +0100

    Redshift function uses random port for tunnel

--- dots/fish/.config/fish/functions/redshift.fish
+++ dots/fish/.config/fish/functions/redshift.fish
@@ -1,5 +1,6 @@
 function redshift --no-scope-shadowing
-    ssh -f -o ExitOnForwardFailure=yes bastion sleep 10
+    set port (random 10000 20000)
+    ssh -f -o ExitOnForwardFailure=yes -L $port:redshift.eporta-internal:5439 bastion sleep 10
     set -x PGPASSWORD (aws ssm get-parameter --name "/prod/redshift/password" --with-decryption --output text | awk '{print $6}')
-    psql -h localhost -p 5439 -U app_redshift analytics
+    psql -h localhost -p $port -U app_redshift analytics
 end
