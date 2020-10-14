0318cf991f4ea543f950246923880d4d1dc65ab2
mwagg <michael@guerillatactics.co.uk>
2020-10-14 10:28:37 +0100

    Add script to connect to transactional-db

---
+++ dots/fish/.config/fish/functions/transactional-db.fish
@@ -0,0 +1,6 @@
+function transactional-db --no-scope-shadowing
+    set port (random 10000 20000)
+    ssh -L $port:transactional-db.eporta-internal:5432 -f -o ExitOnForwardFailure=yes bastion sleep 10
+    set -x PGPASSWORD (aws ssm get-parameter --name "/transactional/database/url" --with-decryption --output text | awk '{print $7}' | sed -e 's/postgres:\/\/transactional:\(.*\)@.*/\1/')
+    psql -h localhost -p $port -U transactional transactional
+end
