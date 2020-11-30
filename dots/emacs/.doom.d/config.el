9509d1190abb9aa1f26fe76e81ce0274099f6b66
mwagg <michael@guerillatactics.co.uk>
2020-11-30 18:21:00 +0000

    Update projectile root files for js

--- dots/emacs/.doom.d/config.el
+++ dots/emacs/.doom.d/config.el
@@ -91,3 +91,6 @@
   :config
   (setq poetry-tracking-strategy 'projectile)
   (add-hook 'python-mode #'poetry-tracking-mode))
+
+(after! projectile
+  (pushnew! projectile-project-root-files-top-down-recurring "package.json"))
