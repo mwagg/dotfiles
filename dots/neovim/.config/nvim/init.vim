3f7e002a9cfc15da6ee410e65d07a4fac7efae4b
mwagg <michael@guerillatactics.co.uk>
2021-03-14 16:42:25 +0000

    Add packer and lua config file

--- dots/neovim/.config/nvim/init.vim
+++ dots/neovim/.config/nvim/init.vim
@@ -1,2 +1,4 @@
 source ~/.config/nvim/settings.vim
 source ~/.config/nvim/mappings.vim
+
+luafile ~/.config/nvim/new-init.lua
---
+++ dots/neovim/.config/nvim/new-init.lua
@@ -0,0 +1,11 @@
+require('plugins')
+
+-- -- LSP
+-- require('lsp/lua')
+-- require('lsp/typescript')
+
+-- -- plugins
+-- require('plugins/lsp-config')
+-- require('plugins/lspsaga')
+-- require('plugins/telescope')
+
