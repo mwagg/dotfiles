4a4225d7ee6a60ab755a21c239a4d7be91ff4515
mwagg <michael@guerillatactics.co.uk>
2021-03-14 16:53:28 +0000

    Add neogit

--- dots/neovim/.config/nvim/lua/plugins.lua
+++ dots/neovim/.config/nvim/lua/plugins.lua
@@ -13,4 +13,5 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   -- Packer can manage itself as an optional plugin
   use {'wbthomason/packer.nvim', opt = true}
+  use 'TimUntersberger/neogit'
 end)
