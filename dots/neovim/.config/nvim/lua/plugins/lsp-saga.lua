ecfce2410447c4aef682792db7202b033d528dd9
mwagg <michael@guerillatactics.co.uk>
2021-03-16 19:02:01 +0000

    Swap binding for next/prev diagnostic

--- dots/neovim/.config/nvim/lua/plugins/lsp-saga.lua
+++ dots/neovim/.config/nvim/lua/plugins/lsp-saga.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '[e', ":Lspsaga diagnostic_jump_next<CR>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', ']e', ":Lspsaga diagnostic_jump_prev<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', ']e', ":Lspsaga diagnostic_jump_next<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '[e', ":Lspsaga diagnostic_jump_prev<CR>", { silent = true, noremap = true })
