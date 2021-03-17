9d640a0842d65f8ae4edc5ca12028991b20649b1
mwagg <michael@guerillatactics.co.uk>
2021-03-17 19:12:43 +0000

    Remove black plugin

--- dots/neovim/.config/nvim/lua/plugins.lua
+++ dots/neovim/.config/nvim/lua/plugins.lua
@@ -1,11 +1,12 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
+    execute('!git clone https://github.com/wbthomason/packer.nvim ' ..
+                install_path)
+    execute 'packadd packer.nvim'
 end
 
 vim.cmd [[packadd packer.nvim]]
@@ -13,53 +14,52 @@ local packer = require('packer')
 local use = packer.use
 
 return packer.startup(function()
-  -- Packer can manage itself as an optional plugin
-  use {'wbthomason/packer.nvim', opt = true}
-  -- git
-  use 'TimUntersberger/neogit'
-  use 'rhysd/git-messenger.vim'
-  use 'airblade/vim-gitgutter'
-  -- which key
-  use 'liuchengxu/vim-which-key'
-  -- theme
-  use 'sickill/vim-monokai'
-  -- comments
-  use 'tpope/vim-commentary'
-  -- comments when mixed languages in file
-  use 'suy/vim-context-commentstring'
-  -- set the root dir automatically
-  use 'airblade/vim-rooter'
-  -- better whitespace
-  use 'ntpeters/vim-better-whitespace'
-  -- quick jump
-  use 'justinmk/vim-sneak'
-  -- surround
-  use 'tpope/vim-surround'
-  -- better syntax
-  use 'sheerun/vim-polyglot'
-  -- icons
-  use 'ryanoasis/vim-devicons'
-  -- auto pairs for '(' '[' '{'
-  use 'jiangmiao/auto-pairs'
-  -- closetags
-  use 'alvan/vim-closetag'
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'glepnir/lspsaga.nvim'
-  use 'onsails/lspkind-nvim'
-  -- telescope
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  -- nvim tree
-  use 'kyazdani42/nvim-tree.lua'
-  -- tab line
-  use 'kyazdani42/nvim-web-devicons'
-  use 'romgrk/barbar.nvim'
-  -- floaterm
-  use 'voldikss/vim-floaterm'
-  -- completion
-  use 'nvim-lua/completion-nvim'
-  -- python
-  use { 'psf/black', branch= 'stable' }
+    -- Packer can manage itself as an optional plugin
+    use {'wbthomason/packer.nvim', opt = true}
+    -- git
+    use 'TimUntersberger/neogit'
+    use 'rhysd/git-messenger.vim'
+    use 'airblade/vim-gitgutter'
+    -- which key
+    use 'liuchengxu/vim-which-key'
+    -- theme
+    use 'sickill/vim-monokai'
+    -- comments
+    use 'tpope/vim-commentary'
+    -- comments when mixed languages in file
+    use 'suy/vim-context-commentstring'
+    -- set the root dir automatically
+    use 'airblade/vim-rooter'
+    -- better whitespace
+    use 'ntpeters/vim-better-whitespace'
+    -- quick jump
+    use 'justinmk/vim-sneak'
+    -- surround
+    use 'tpope/vim-surround'
+    -- better syntax
+    use 'sheerun/vim-polyglot'
+    -- icons
+    use 'ryanoasis/vim-devicons'
+    -- auto pairs for '(' '[' '{'
+    use 'jiangmiao/auto-pairs'
+    -- closetags
+    use 'alvan/vim-closetag'
+    -- LSP
+    use 'neovim/nvim-lspconfig'
+    use 'glepnir/lspsaga.nvim'
+    use 'onsails/lspkind-nvim'
+    -- telescope
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-media-files.nvim'
+    -- nvim tree
+    use 'kyazdani42/nvim-tree.lua'
+    -- tab line
+    use 'kyazdani42/nvim-web-devicons'
+    use 'romgrk/barbar.nvim'
+    -- floaterm
+    use 'voldikss/vim-floaterm'
+    -- completion
+    use 'nvim-lua/completion-nvim'
 end)
--- dots/neovim/.config/nvim/lua/plugins/python.lua
+++
@@ -1,1 +0,0 @@
-vim.cmd("autocmd BufWritePre *.py execute ':Black'")
