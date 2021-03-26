36f90730f68bc16146dc84e9b1f8f2894f41ca09
mwagg <michael@guerillatactics.co.uk>
2021-03-26 12:16:52 +0000

    Add desktop-changer extension

--- setup.sh
+++ setup.sh
@@ -167,6 +167,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 
 # gnome extensions
 yay_install gnome-shell-frippery
+yay_install gnome-shell-extension-desk-changer
 
 # gtk
 yay_install qogir-icon-theme-git
