16a3de97f392546b2a37f1f8d5eb7631a3c2a1e7
mwagg <michael@guerillatactics.co.uk>
2021-04-15 18:47:27 +0100

    Increase timeout for terraform formatting

--- dots/neovim/.config/nvim/lua/lsp/terraform.lua
+++ dots/neovim/.config/nvim/lua/lsp/terraform.lua
@@ -1,3 +1,3 @@
-require'lspconfig'.terraformls.setup {}
+require'lspconfig'.terraformls.setup {filetypes = {"terraform", "tf"}}
 
-vim.cmd('autocmd BufWritePre *.tf :lua vim.lsp.buf.formatting_sync(nil, 100)')
+vim.cmd('autocmd BufWritePre *.tf :lua vim.lsp.buf.formatting_sync(nil, 1000)')
