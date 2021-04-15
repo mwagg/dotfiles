15d5a0eb5444a96f103927ff7ca566b83ee5d83d
mwagg <michael@guerillatactics.co.uk>
2021-04-15 19:03:29 +0100

    Switch barbar keys to use TAB

--- dots/neovim/.config/nvim/lua/plugins/barbar.lua
+++ dots/neovim/.config/nvim/lua/plugins/barbar.lua
@@ -1,8 +1,8 @@
 vim.g.bufferline = {animation = true, auto_hide = true}
 
-vim.api.nvim_set_keymap('n', '<A-,>', ":BufferPrevious<cr>",
+vim.api.nvim_set_keymap('n', '<S-TAB>', ":BufferPrevious<cr>",
                         {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', '<A-.>', ":BufferNext<cr>",
+vim.api.nvim_set_keymap('n', '<TAB>', ":BufferNext<cr>",
                         {silent = true, noremap = true})
 
 local wk = require('whichkey_setup')
