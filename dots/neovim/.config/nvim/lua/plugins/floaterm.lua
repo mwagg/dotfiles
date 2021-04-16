67cc405dae4cc81c9d6286145446b4f500c6e66e
mwagg <michael@guerillatactics.co.uk>
2021-04-16 19:28:59 +0100

    Make floaterm use split

--- dots/neovim/.config/nvim/lua/plugins/floaterm.lua
+++ dots/neovim/.config/nvim/lua/plugins/floaterm.lua
@@ -1,6 +1,7 @@
 vim.g.floaterm_autoclose = 1
-vim.g.floaterm_width = 0.8
+vim.g.floaterm_width = 0.5
 vim.g.floaterm_height = 0.8
+vim.g.floaterm_wintype = 'vsplit'
 
 local wk = require('whichkey_setup')
 
