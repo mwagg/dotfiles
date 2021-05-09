b16d15b2d4805a46520119519e9ac2774b5fb655
mwagg <michael@guerillatactics.co.uk>
2021-05-09 17:46:26 +0100

    Add fd

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -23,7 +23,8 @@ sudo dnf install -y \
 	chromium \
     ripgrep \
 	gnome-tweaks \
-	entr
+	entr \
+	fd-find
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
