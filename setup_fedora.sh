20d45bc5df329167ac89d6c3cda2f29d95e87587
mwagg <michael@guerillatactics.co.uk>
2021-05-17 14:11:28 +0100

    Add htop

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -25,7 +25,8 @@ sudo dnf install -y \
 	gnome-tweaks \
 	entr \
 	fd-find \
-	openssh-server
+	openssh-server \
+	htop
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
