f06f8e0daa669e435ad6dc17d4feafa60fff2185
mwagg <michael@guerillatactics.co.uk>
2021-05-18 09:17:52 +0100

    Add haproxy

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -32,7 +32,8 @@ sudo dnf install -y \
 	openssh-server \
 	htop \
 	terraform \
-	awscli
+	awscli \
+	haproxy
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
