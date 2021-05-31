111682d660c9ef5e83e0206c358021055df85915
mwagg <michael@guerillatactics.co.uk>
2021-05-31 21:00:56 +0100

    Enable spell check when writing commit messages

--- dots/shared/neovim/.config/nvim/lua/plugins/neogit.lua
+++ dots/shared/neovim/.config/nvim/lua/plugins/neogit.lua
@@ -1,3 +1,8 @@
 local neogit = require('neogit')
 
 neogit.setup {}
+
+vim.cmd('augroup neogit_spelling')
+vim.cmd('autocmd!')
+vim.cmd('autocmd filetype gitcommit set spell')
+vim.cmd('augroup END')
