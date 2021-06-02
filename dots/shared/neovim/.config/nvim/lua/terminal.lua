7cadda9b70c4c75bb50e8efc87879781ef97fdff
mwagg <michael@guerillatactics.co.uk>
2021-06-02 19:25:01 +0100

    Don't show signcolumn in terminals in neovim

--- dots/shared/neovim/.config/nvim/lua/terminal.lua
+++ dots/shared/neovim/.config/nvim/lua/terminal.lua
@@ -2,3 +2,4 @@
 vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')
 vim.cmd('autocmd TermOpen * startinsert')
 vim.cmd("autocmd BufWinEnter,WinEnter term://* startinsert")
+vim.cmd("autocmd TermOpen * setlocal signcolumn=no")
