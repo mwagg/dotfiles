9c84a80e99d0179db72877110a3aa672b12cebd3
mwagg <michael@guerillatactics.co.uk>
2021-06-03 19:33:46 +0100

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+trap 'rm -rf /tmp/key' EXIT
+
+export PATH=$PATH:~/.local/bin
+
 mkdir -p ~/.local/bin
 type bw || { curl -L -o /tmp/bw.zip 'https://vault.bitwarden.com/download/?app=cli&platform=linux'; unzip /tmp/bw.zip -d ~/.local/bin/; chmod +x ~/.local/bin/bw; }
 export BW_SESSION="$(bw login --raw)"
 export BW_SESSION="$(bw unlock --raw)"
 
-bw get attachment 9p6xj344ur9dce8y93msj2gsstem3ml3 --itemid dcedc674-089c-4f7f-a7d1-aa8e00b8b5ed --raw | gpg --import
+bw get attachment 9p6xj344ur9dce8y93msj2gsstem3ml3 --itemid dcedc674-089c-4f7f-a7d1-aa8e00b8b5ed --raw > /tmp/key
+gpg --import /tmp/key
 bw get attachment qgarlw2wotzg18s4etkyt3jjo47dsnfh --itemid dcedc674-089c-4f7f-a7d1-aa8e00b8b5ed --raw | gpg --import-ownertrust
 
 bw get attachment iazv38i80dl0gq9ctn80p0zcoizlaglx --itemid e93fa440-324a-419e-acd4-aa7300d8ed5d --output ~/.ssh/eporta.pub
