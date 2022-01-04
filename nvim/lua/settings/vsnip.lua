b64161cd3216442aaf12e3d4863bc7095bea3c0f
Mike Wagg <mike.wagg@shopify.com>
2022-01-04 21:21:58 +0000

    Configure vsnip mappings

--- nvim/lua/settings/vsnip.lua
+++ nvim/lua/settings/vsnip.lua
@@ -1,3 +1,11 @@
 local use = require("packer").use
 
-use {"hrsh7th/vim-vsnip"}
+use {
+    "hrsh7th/vim-vsnip",
+    config = function()
+        vim.cmd("imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'")
+        vim.cmd("smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'")
+        vim.cmd("imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'")
+        vim.cmd("smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'")
+    end
+}
