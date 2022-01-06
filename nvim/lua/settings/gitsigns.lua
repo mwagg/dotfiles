09f9f54e9ca545a22de0a74dac208576353610b2
Mike Wagg <mike.wagg@shopify.com>
2022-01-06 10:00:05 +0000

    Remove gitsigns mappings
    These now come from LSP

--- nvim/lua/settings/gitsigns.lua
+++ nvim/lua/settings/gitsigns.lua
@@ -3,7 +3,6 @@ local use = require("packer").use
 use {
     "lewis6991/gitsigns.nvim",
     requires = {'nvim-lua/plenary.nvim'},
-    after = "which-key.nvim",
     config = function()
         require('gitsigns').setup({
             keymaps = {
@@ -14,16 +13,5 @@ use {
                 ['n [c'] = {expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'"}
             }
         })
-        require('which-key').register({
-            g = {
-                s = {"<cmd>Gitsigns stage_hunk<CR>", "Stage hunk"},
-                u = {"<cmd>Gitsigns undo_stage_hunk<CR>", "Undo stage hunk"},
-                r = {"<cmd>Gitsigns reset_hunk<CR>", "Reset hunk"},
-                R = {"<cmd>Gitsigns reset_buffer<CR>", "Reset buffer"},
-                p = {"<cmd>Gitsigns preview_hunk<CR>", "Preview hunk"},
-                S = {"<cmd>Gitsigns stage_buffer<CR>", "Stage buffer"},
-                U = {"<cmd>Gitsigns reset_buffer_index<CR>", "Reset buffer index"}
-            }
-        }, {prefix = "<leader>"})
     end
 }
