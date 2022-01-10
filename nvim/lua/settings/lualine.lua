c12fb6d5589d904022d87a9d50e8b209ca1b46e7
Mike Wagg <mike.wagg@shopify.com>
2022-01-10 18:16:09 +0000

    Show current scope from TS in lualine

--- nvim/lua/settings/lualine.lua
+++ nvim/lua/settings/lualine.lua
@@ -1,9 +1,24 @@
 local use = require("packer").use
 
 use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true},
+    "nvim-lualine/lualine.nvim",
+    requires = {"kyazdani42/nvim-web-devicons", "SmiteshP/nvim-gps", opt = true},
     config = function()
-        require('lualine').setup {options = {theme = "nightfox"}}
+        local gps = require("nvim-gps")
+
+        gps.setup()
+
+        -- Returns a string with context information
+        require("lualine").setup {
+            options = {theme = "nightfox"},
+            sections = {
+                lualine_a = {"mode"},
+                lualine_b = {"branch", "diff", "diagnostics"},
+                lualine_c = {"filename", {gps.get_location, cond = gps.is_available}},
+                lualine_x = {"encoding", "fileformat", "filetype"},
+                lualine_y = {"progress"},
+                lualine_z = {"location"}
+            }
+        }
     end
 }
