510e0f2008c90337c8526e68a83bf2effe523b28
Mike Wagg <mike.wagg@shopify.com>
2022-01-10 18:15:46 +0000

    Add TS symbol definition/usage highlight
    Via nvim-treesitter-refactor plugin

--- nvim/lua/settings/treesitter.lua
+++ nvim/lua/settings/treesitter.lua
@@ -2,6 +2,7 @@ local use = require("packer").use
 
 use {
     "nvim-treesitter/nvim-treesitter",
+    requires = "nvim-treesitter/nvim-treesitter-refactor",
     config = function()
         require("nvim-treesitter.configs").setup {
             ensure_installed = "all",
@@ -22,7 +23,8 @@ use {
                 enable = true,
                 extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
                 max_file_lines = 2000 -- Do not enable for files with more than specified lines
-            }
+            },
+            refactor = {highlight_definitions = {enable = true}}
         }
     end,
     run = ":TSUpdate"
