5d2b967e8792365b470a0443c33120d6c182d3be
Mike <mike.wagg@shopify.com>
2022-02-20 15:07:09 +0000

    Add mini.jump

--- nvim/lua/settings/mini.lua
+++ nvim/lua/settings/mini.lua
@@ -4,6 +4,7 @@ use {
     "echasnovski/mini.nvim",
     config = function()
         require("mini.indentscope").setup({})
+        require("mini.jump").setup({})
         require("mini.surround").setup({})
     end
 }
