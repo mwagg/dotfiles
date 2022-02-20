df45149f57e1de61263563dbee228b62a884aefa
Mike <mike.wagg@shopify.com>
2022-02-20 15:06:57 +0000

    Add mini.indentscope

--- nvim/lua/settings/mini.lua
+++ nvim/lua/settings/mini.lua
@@ -3,6 +3,7 @@ local use = require("packer").use
 use {
     "echasnovski/mini.nvim",
     config = function()
+        require("mini.indentscope").setup({})
         require("mini.surround").setup({})
     end
 }
