e5b6ca224d31e0aa64e6e276cc5e4c1983dbf4ae
Mike Wagg <mike.wagg@shopify.com>
2022-02-21 15:04:38 +0000

    Don't install treesitter for swift or phpdoc

--- nvim/lua/settings/treesitter.lua
+++ nvim/lua/settings/treesitter.lua
@@ -6,7 +6,7 @@ use {
     config = function()
         require("nvim-treesitter.configs").setup {
             ensure_installed = "all",
-            ignore_install = {"haskell"},
+            ignore_install = {"haskell", "swift", "phpdoc"},
             highlight = {enable = true, additional_vim_regex_highlighting = false},
             incremental_selection = {
                 enable = true,
