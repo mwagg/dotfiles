4d702f50528362e472ff487d7abe8cd6564727a5
Mike <mike.wagg@shopify.com>
2022-03-26 22:38:42 +0000

    Switch to duskfox neovim theme

--- nvim/lua/settings/nightfox.lua
+++ nvim/lua/settings/nightfox.lua
@@ -3,6 +3,6 @@ local use = require("packer").use
 use {
     "EdenEast/nightfox.nvim",
     config = function()
-        require('nightfox').load("nightfox")
+        vim.cmd("colorscheme duskfox")
     end
 }
