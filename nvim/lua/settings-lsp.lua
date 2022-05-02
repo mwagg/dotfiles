9c72d3c5cd5b7735b1dc6b8d934d9dfd1e4780ec
Mike <mike.wagg@shopify.com>
2022-05-02 20:47:42 +0100

    Update lsp code to use nvim-lsp-installer

--- nvim/lua/settings-lsp.lua
+++ nvim/lua/settings-lsp.lua
@@ -3,7 +3,7 @@ local use = require("packer").use
 use {
     "ray-x/lsp_signature.nvim",
     config = function()
-        require"lsp_signature".setup()
+        require "lsp_signature".setup()
     end
 }
 
@@ -11,16 +11,18 @@ use {
     "neovim/nvim-lspconfig",
     requires = {
         "tami5/lspsaga.nvim", "hrsh7th/cmp-nvim-lsp", "jose-elias-alvarez/null-ls.nvim",
-        "jose-elias-alvarez/nvim-lsp-ts-utils"
+        "jose-elias-alvarez/nvim-lsp-ts-utils", "williamboman/nvim-lsp-installer"
     },
+    after = "which-key.nvim",
     config = function()
         local lspconfig = require "lspconfig"
+        require("nvim-lsp-installer").setup({ automatic_installation = true })
 
         -- ******************************
         -- LSP config
         -- ******************************
-        require'lspsaga'.init_lsp_saga({
-            code_action_keys = {quit = '<esc>', exec = '<CR>'},
+        require 'lspsaga'.init_lsp_saga({
+            code_action_keys = { quit = '<esc>', exec = '<CR>' },
             finder_action_keys = {
                 open = '<cr>',
                 vsplit = 's',
@@ -31,114 +33,74 @@ use {
             }
         })
 
-        local common_on_attach = function(client, bufnr)
+        local on_attach = function(client, bufnr)
             local function buf_set_keymap(...)
                 vim.api.nvim_buf_set_keymap(bufnr, ...)
             end
 
             -- Mappings.
-            local opts = {noremap = true, silent = true}
+            local opts = { noremap = true, silent = true }
 
-            buf_set_keymap('n', '<leader>ca', "<cmd>lua require('lspsaga.codeaction').code_action()<cr>", opts)
+            require("which-key").register({
+                c = {
+                    a = { ":lua require('lspsaga.codeaction').code_action()<cr>", "Code action" },
+                    r = { ":lua require('lspsaga.hover').render_hover_doc()<CR>", "Rename" },
+                    f = { ":lua vim.lsp.buf.formatting()<CR>", "Format" },
+                }
+            }, { prefix = "<leader>", buffer = bufnr })
+
             buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
             buf_set_keymap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
             buf_set_keymap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
-            buf_set_keymap('n', '<leader>cr', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
             buf_set_keymap('n', '[e', "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
             buf_set_keymap('n', ']e', "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
             buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-            buf_set_keymap('n', '<leader>cf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-            buf_set_keymap('n', '<leader>ch', "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", opts)
 
             if client.resolved_capabilities.document_formatting then
-                local cmd = vim.api.nvim_command
-                cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+                vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+                    pattern = "<buffer>",
+                    callback = function()
+                        vim.lsp.buf.formatting()
+                    end
+                })
             end
         end
 
         local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-        -- EFM
-        lspconfig.efm.setup({
-            cmd = {"efm-langserver"},
-            init_options = {documentFormatting = true, codeAction = false},
-            filetypes = {"lua"},
-            settings = {
-                rootMarkers = {".git/"},
-                languages = {
-                    lua = {
-                        {
-                            formatCommand = "lua-format -i --no-keep-simple-function-one-line --column-limit=120",
-                            formatStdin = true
-                        }
-                    }
-                }
-            }
-        })
-
         -- Lua
         local runtime_path = vim.split(package.path, ';')
         table.insert(runtime_path, "lua/?.lua")
         table.insert(runtime_path, "lua/?/init.lua")
-        lspconfig.sumneko_lua.setup({
+        lspconfig.sumneko_lua.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
             settings = {
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            -- Tell the language server which version of Lua you"re using (most likely LuaJIT in the case of Neovim)
-                            version = "LuaJIT",
-                            -- Setup your lua path
-                            path = vim.split(package.path, ";")
-                        },
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` global
-                            globals = {"vim"}
-                        },
-                        workspace = {
-                            -- Make the server aware of Neovim runtime files
-                            library = {
-                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
-                            },
-                            maxPreload = 10000
-                        }
-                    }
-                }
-            },
-            on_attach = common_on_attach
-        })
-
-        -- Nix
-        -- lspconfig.rnix.setup({on_attach = common_on_attach, capabilities = capabilities})
-
-        -- JS/TS
-        lspconfig.tsserver.setup({
-            on_attach = function(client, bufnr)
-                client.resolved_capabilities.document_formatting = false
-                client.resolved_capabilities.document_range_formatting = false
-
-                local ts_utils = require("nvim-lsp-ts-utils")
-                ts_utils.setup({})
-                ts_utils.setup_client(client)
-
-                common_on_attach(client, bufnr)
-            end,
-            capabilities = capabilities
-        })
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.diagnostics.eslint.with({prefer_local = "node_modules/.bin"}),
-                null_ls.builtins.code_actions.eslint.with({prefer_local = "node_modules/.bin"}),
-                null_ls.builtins.formatting.prettier.with({prefer_local = "node_modules/.bin"}),
-                null_ls.builtins.code_actions.gitsigns
-            },
-            on_attach = common_on_attach
-        })
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT',
+                        -- Setup your lua path
+                        path = runtime_path,
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { 'vim' },
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                        enable = false,
+                    },
+                },
+            }
+        }
 
         -- Ruby - sorbet
-        lspconfig.sorbet.setup({on_attach = common_on_attach, capabilities = capabilities})
-        lspconfig.solargraph.setup({on_attach = common_on_attach, capabilities = capabilities})
+        lspconfig.sorbet.setup({ on_attach = on_attach, capabilities = capabilities })
+        lspconfig.solargraph.setup({ on_attach = on_attach, capabilities = capabilities })
     end
 }
