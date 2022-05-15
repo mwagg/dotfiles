7950a4a666bf9d0a248c672506840d434dd52f6d
Mike Wagg <mike.wagg@shopify.com>
2022-05-15 14:43:53 +0100

    Add reloading function and move options to module

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,16 @@
+function _G.ReloadConfig()
+  for name, _ in pairs(package.loaded) do
+    if name:match('^mwagg') then
+      package.loaded[name] = nil
+    end
+  end
+
+  dofile(vim.env.MYVIMRC)
+end
+
+vim.cmd('command! ReloadConfig lua ReloadConfig()')
+
 require("plugins")
 require("mappings")
-require("options")
+require("mwagg/options")
 require("autocmd")
