64bacfa0d43ee3e0c627b2a29eb0d53f3974f3a5
Mike Wagg <mike.wagg@shopify.com>
2022-07-21 17:04:58 +0100

    Use toggle term for terminals

--- nvim/lua/mwagg/terminal.lua
+++ nvim/lua/mwagg/terminal.lua
@@ -1,15 +1,24 @@
-local use = require('packer').use;
+local use = require("packer").use;
+
+function TerminalCommand(cmd)
+  vim.cmd("terminal " .. cmd)
+end
 
 use {
-  "numToStr/FTerm.nvim",
+  "akinsho/toggleterm.nvim",
   config = function()
-    vim.keymap.set("n", "<leader>``", function()
-      require('FTerm').toggle()
+    local term = require("toggleterm")
+    term.setup({
+      start_in_insert = true
+    })
+
+    vim.keymap.set("n", "<leader>;", function()
+      vim.cmd("ToggleTerm")
     end, { desc = "Toggle terminal" })
   end
 }
 
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 vim.cmd("autocmd TermOpen * setlocal signcolumn=no") -- no sign column in terminals
 vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber") -- no line numbers in terminals
