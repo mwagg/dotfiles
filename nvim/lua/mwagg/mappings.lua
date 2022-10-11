99ec4e1948714d9375092410bf70fb7b60e20c89
Mike Wagg <mike.wagg@shopify.com>
2022-10-11 09:39:16 +0100

    Add mapping for buffer delete

--- nvim/lua/mwagg/mappings.lua
+++ nvim/lua/mwagg/mappings.lua
@@ -23,3 +23,6 @@ vim.keymap.set("n", "<Down>", ":resize +1<CR>")
 
 -- source current file as lua
 vim.keymap.set("n", "<leader>fl", ":luafile %<CR>")
+
+-- buffers
+vim.keymap.set("n", "<leader>bd", ":bdelete<cr>", { desc = "Delete" })
