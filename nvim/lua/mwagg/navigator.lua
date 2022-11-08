2fc2dd2fe8ec1756622ca2a70fc6bc26e50ebc3b
Mike <mike.wagg@shopify.com>
2022-11-08 13:28:37 +0000

    Add navigator to nvim

---
+++ nvim/lua/mwagg/navigator.lua
@@ -0,0 +1,12 @@
+local use = require("packer").use
+
+use({
+  'ray-x/navigator.lua',
+  requires = {
+    { 'ray-x/guihua.lua', run = 'cd lua/fzy && make' },
+    { 'neovim/nvim-lspconfig' },
+  },
+  config = function()
+    require('navigator').setup()
+  end
+})
