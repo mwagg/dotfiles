e2863f20fc1cb82a02f7373d141cd978a78dfcf7
Mike Wagg <mike.wagg@shopify.com>
2022-11-10 08:50:06 +0000

    Remove ft files

--- nvim/ftplugin/ruby.lua
+++
@@ -1,9 +0,0 @@
-require("key-menu").set("n", "<leader>r", { desc = "Ruby", buffer = true })
-
-vim.keymap.set("n", "<leader>rc", function()
-  vim.cmd("belowright split term://./bin/rails c")
-end, { desc = "rails console", buffer = true })
-
-vim.keymap.set("n", "<leader>rs", function()
-  vim.cmd("belowright split term://./bin/dev")
-end, { desc = "Rails dev server", buffer = true })
