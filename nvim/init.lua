5647fe67f79f332da95836f5ff322676e56af1ec
Mike <mike.wagg@shopify.com>
2022-12-22 17:08:47 +0000

    Add terminal settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -150,6 +150,23 @@ vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
 
+-- terminal window navigation
+vim.keymap.set('t', '<C-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('t', '<C-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('t', '<C-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('t', '<C-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('i', '<C-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('i', '<C-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('i', '<C-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('i', '<C-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- terminal settings
+vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber") -- no line numbers in terminals
+vim.cmd("autocmd TermOpen * startinsert") -- switch to insert mode when opening a terminal
+vim.cmd("autocmd TermOpen * setlocal signcolumn=no") -- no sign column in terminals
+vim.keymap.set("n", "<leader>`", ":terminal<cr>", { desc = "Open terminal in this buffer" })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
