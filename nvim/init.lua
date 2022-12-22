6a65fb1b5e932d62f54c354f656829d37c702336
Mike <mike.wagg@shopify.com>
2022-12-22 17:08:58 +0000

    Ensure vim treesitter is installed

--- nvim/init.lua
+++ nvim/init.lua
@@ -252,7 +252,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'ruby' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'ruby', 'vim' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
