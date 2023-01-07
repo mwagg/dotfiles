132422bebc71f9fa53607d436c32bd1040599d32
Mike <mike.wagg@shopify.com>
2023-01-07 21:29:10 +0000

    Use extra mini features

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -6,5 +6,11 @@ return {
     require("mini.surround").setup({})
     require("mini.starter").setup({})
     require("mini.comment").setup({})
+    require("mini.trailspace").setup({})
+    require("mini.cursorword").setup({})
+    require("mini.jump2d").setup({})
+    require("mini.animate").setup({})
+
+    vim.keymap.set("n", "<leader>j", MiniJump2d.start, { desc = "Jump to" })
   end
 }
