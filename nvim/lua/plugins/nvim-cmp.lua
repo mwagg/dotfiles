3e19919793c7ce15278bc317633be72a61d21f07
Mike Wagg <mike.wagg@shopify.com>
2023-04-27 12:19:15 +0100

    Updated nvim-cmp mappings

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -7,7 +7,8 @@ return {
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+      ["<Tab>"] = cmp.mapping.confirm({ cmp.ConfirmBehavior.Insert, select = true }),
+      ["<Cr>"] = cmp.mapping.confirm({ cmp.ConfirmBehavior.Insert, select = true }),
     })
 
     return opts
