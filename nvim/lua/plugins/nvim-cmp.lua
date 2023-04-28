3291159e24ecaeca87270993772943937259a12d
Mike <mike.wagg@shopify.com>
2023-04-28 10:14:09 +0100

    Add Copilot to neovim

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -7,7 +7,6 @@ return {
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<Tab>"] = cmp.mapping.confirm({ cmp.ConfirmBehavior.Insert, select = true }),
       ["<Cr>"] = cmp.mapping.confirm({ cmp.ConfirmBehavior.Insert, select = true }),
     })
 
