b85ab5d3d7a0a94d9dcc03cca1bfc2ce49616e81
Mike Wagg <mike.wagg@shopify.com>
2023-05-26 14:44:14 +0100

    Replace vim-test with neotest

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,5 +2,5 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber") -- no line numbers in terminals
-vim.cmd("autocmd TermOpen * startinsert") -- switch to insert mode when opening a terminal
+vim.cmd("autocmd TermOpen *  if nvim_buf_get_name(0) =~# '^term://.*' | startinsert | endif") -- switch to insert mode when opening a terminal
 vim.cmd("autocmd TermOpen * setlocal signcolumn=no") -- no sign column in terminals
