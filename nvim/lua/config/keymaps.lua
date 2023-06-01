69b62c622c4247516fe7dc9f285baa116ceda555
Mike <mike.wagg@shopify.com>
2023-06-01 11:59:32 +0100

    Clear noice when pressing esc

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- vim.keymap.set("n", "<Esc>", ":Noice dismiss<cr>")
+vim.keymap.set("n", "<Esc>", function()
+  require("noice.view.backend.notify").dismiss()
+end)
