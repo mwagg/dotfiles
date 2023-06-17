47290b4e75ceed1d963201e66d038c8b236db3d6
Mike <mike.wagg@shopify.com>
2023-06-17 15:36:50 +0100

    Add eslint and copilot to neovim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,6 +15,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
