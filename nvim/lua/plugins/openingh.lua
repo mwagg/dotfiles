d2ef3d2cc45977b33e6bddb7d4746bc6915c5f84
Mike Wagg <mike.wagg@shopify.com>
2023-06-21 16:00:58 +0100

    Add keymaps for open file/repo in github

--- nvim/lua/plugins/openingh.lua
+++ nvim/lua/plugins/openingh.lua
@@ -1,4 +1,8 @@
 return {
   "Almo7aya/openingh.nvim",
   cmd = { "OpenInGHRepo", "OpenInGHFile" },
+  keys = {
+    { "<leader>go", ":OpenInGHFile<cr>", desc = "Open this file in GitHub" },
+    { "<leader>gO", ":OpenInGHRepo<cr>", desc = "Open this repo in GitHub" },
+  },
 }
