8ab7b2e6418a2b66319fc6077f8f59876c2d29ea
Mike Wagg <mike.wagg@shopify.com>
2023-07-07 11:02:25 +0100

    Add treesitter-endwise

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    opts.endwise = { enable = true }
     table.insert(opts.ensure_installed, "ruby")
   end,
+  dependencies = {
+    "RRethy/nvim-treesitter-endwise",
+  },
 }
