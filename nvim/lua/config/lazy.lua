a0b379c2178c09d4dc41e6e6115ce9c578a050fa
Mike Wagg <mike.wagg@shopify.com>
2023-07-12 17:45:05 +0100

    Remove lazy ruby extras

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,7 +14,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
