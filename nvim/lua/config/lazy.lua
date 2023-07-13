48404d4b27e739c67a12af4116dd75dc8af148c4
Mike <mike.wagg@shopify.com>
2023-07-13 20:55:28 +0100

    Remove flash extra

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
