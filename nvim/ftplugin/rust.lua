021412667d61111e282d95eb773dc7e245867e38
Mike Wagg <mike.wagg@shopify.com>
2023-12-08 16:26:36 +0000

    Add mapping for rust docs

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -6,3 +6,17 @@ vim.keymap.set("n", "<leader>rr", tmux_term.run({ name = "Cargo run", cmd = "car
   desc = "Cargo run",
   buffer = 0,
 })
+vim.keymap.set(
+  "n",
+  "<leader>rd",
+  tmux_term.run({
+    name = "Rust docs",
+    cmd = "rustup docs --std",
+    open_as = "pane",
+    read_after_cmd = false,
+  }),
+  {
+    desc = "Rust docs",
+    buffer = 0,
+  }
+)
