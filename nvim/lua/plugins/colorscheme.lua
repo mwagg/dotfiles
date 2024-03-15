4bee66e54047c4a9851fc3e5baebe1233c04f920
Mike <mike.wagg@shopify.com>
2024-03-15 21:28:18 +0000

    Use github colorscheme plugins

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
   {
-    "rafamadriz/neon",
+    'projekt0n/github-nvim-theme',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "neon"
+      colorscheme = "github_dark_dimmed"
     }
   }
 }
