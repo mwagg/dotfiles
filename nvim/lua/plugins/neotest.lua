77b71677b8344bd5edf01791d9efe8bf98163211
Mike Wagg <mike.wagg@shopify.com>
2024-03-21 09:56:16 +0000

    Explicitly install nvim-nio
    This bug is fixed in lazyvim, but not yet released

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,55 +1,59 @@
 return {
-  "nvim-neotest/neotest",
-  keys = {
-    {
-      "<leader>tt",
-      function()
-        local neotest = require("neotest")
-        neotest.output_panel.clear()
-        neotest.run.run()
-      end,
-      desc = "Run nearest test",
-    },
-    {
-      "<leader>tf",
-      function()
-        local neotest = require("neotest")
-        neotest.output_panel.clear()
-        neotest.run.run(vim.fn.expand("%"))
-      end,
-      desc = "Run tests in current file",
-    },
-    {
-      "<leader>to",
-      function()
-        require("neotest").output_panel.toggle()
-      end,
-      desc = "Toggle output panel",
-    },
-    {
-      "<leader>tl",
-      function()
-        local neotest = require("neotest")
-        neotest.output_panel.clear()
-        neotest.run.run_last()
-      end,
-      desc = "Run last test",
-    },
-    {
-      "<leader>ts",
-      function()
-        require("neotest").summary.toggle()
-      end,
-      desc = "Toggle test summary panel",
-    },
-  },
+  { "nvim-neotest/nvim-nio" },
   {
-    "<leader>ta",
-    function()
-      local neotest = require("neotest")
-      neotest.output_panel.clear()
-      neotest.run.attach()
-    end,
-    desc = "Attach to nearest test",
+    "nvim-neotest/neotest",
+    dependencies = { "nvim-neotest/nvim-nio" },
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          local neotest = require("neotest")
+          neotest.output_panel.clear()
+          neotest.run.run()
+        end,
+        desc = "Run nearest test",
+      },
+      {
+        "<leader>tf",
+        function()
+          local neotest = require("neotest")
+          neotest.output_panel.clear()
+          neotest.run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run tests in current file",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "Toggle output panel",
+      },
+      {
+        "<leader>tl",
+        function()
+          local neotest = require("neotest")
+          neotest.output_panel.clear()
+          neotest.run.run_last()
+        end,
+        desc = "Run last test",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Toggle test summary panel",
+      },
+    },
+    {
+      "<leader>ta",
+      function()
+        local neotest = require("neotest")
+        neotest.output_panel.clear()
+        neotest.run.attach()
+      end,
+      desc = "Attach to nearest test",
+    },
   }
 }
