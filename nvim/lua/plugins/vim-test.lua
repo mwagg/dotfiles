df87fdd60d37a94e606d0c7e8f18632cf76794fd
Mike Wagg <mike.wagg@shopify.com>
2024-04-26 19:46:43 +0100

    Write all buffers before running tests

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -7,9 +7,9 @@ return {
     "vim-test/vim-test",
     dependencies = 'preservim/vimux',
     keys = {
-      { "<leader>tt", "<cmd>TestNearest<cr>", desc = "Run nearest test", },
-      { "<leader>tf", "<cmd>TestFile<cr>",    desc = "Run tests in current file", },
-      { "<leader>tl", "<cmd>TestLast<cr>",    desc = "Run last test", },
+      { "<leader>tt", "<cmd>wall<cr><cmd>TestNearest<cr>", desc = "Run nearest test", },
+      { "<leader>tf", "<cmd>wall<cr><cmd>TestFile<cr>",    desc = "Run tests in current file", },
+      { "<leader>tl", "<cmd>wall<cr><cmd>TestLast<cr>",    desc = "Run last test", },
     },
   },
   { 'preservim/vimux' },
