e994229881bd2ebaef47a7a7244d0346cde40484
Mike Wagg <mike.wagg@shopify.com>
2024-06-11 10:43:10 +0100

    Use c-j and c-k in nvim-cmp

--- nvim/lua/plugins/supertab.lua
+++ nvim/lua/plugins/supertab.lua
@@ -11,10 +11,11 @@ return {
     local cmp = require("cmp")
 
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-j>"] = cmp.mapping.select_next_item(),
+      ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          cmp.select_next_item()
+          cmp.confirm({ select = true })
         elseif vim.snippet.active({ direction = 1 }) then
           vim.schedule(function()
             vim.snippet.jump(1)
