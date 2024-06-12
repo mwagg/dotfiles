abeeb7088db1fd8b2fcc0115a1e6f45f58de606c
Mike <mike.wagg@shopify.com>
2024-06-12 20:44:40 +0100

    Add git pull and push keymaps

--- nvim/lua/plugins/mini-git.lua
+++ nvim/lua/plugins/mini-git.lua
@@ -8,6 +8,10 @@ return {
     keys = {
       { "<leader>gc", "<cmd>Git commit<cr>", desc = "Commit" },
       { "<leader>ga", "<cmd>Git commit --amend<cr>", desc = "Amend" },
+      { "<leader>gp", "<cmd>Git pull<cr>", desc = "Pull" },
+      { "<leader>gP", desc = "Push" },
+      { "<leader>gPp", "<cmd>Git push<cr>", desc = "Push" },
+      { "<leader>gPf", "<cmd>Git push --force-with-lease<cr>", desc = "Push (force)" },
     },
   },
 }
