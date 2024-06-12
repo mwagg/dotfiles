078ca96d18bb480d5c6e10bce53ac2cd70b958bc
Mike Wagg <mike.wagg@shopify.com>
2024-06-12 16:32:13 +0100

    Disable spell checking in terminals

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,4 +1,4 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber")                              -- no line numbers in terminals
+vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber nospell") -- no line numbers in terminals
