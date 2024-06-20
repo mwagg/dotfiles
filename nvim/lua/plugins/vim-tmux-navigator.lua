8f38d3fb32db0b2d002e9d79d8b46d134b6b53e3
Mike Wagg <mike.wagg@shopify.com>
2024-06-20 17:57:02 +0100

    Add vim-tmux-navigator

---
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,9 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+  },
+}
