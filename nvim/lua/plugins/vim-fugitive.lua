1dc37641e8fad7c2fb2438218227acee0ac5c091
Mike Wagg <mike.wagg@shopify.com>
2024-07-12 10:38:06 +0100

    Add git log keymap

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,10 +1,11 @@
 return {
   "tpope/vim-fugitive",
-  cmds = { "Git" },
+  cmd = { "Git" },
   keys = {
     { "<Leader>gp", "<cmd> Git pull --rebase<cr>", desc = "Pull" },
     { "<Leader>gP", desc = "Push" },
     { "<Leader>gPp", "<cmd> Git push<cr>", desc = "Push" },
     { "<Leader>gPf", "<cmd> Git push --force-with-lease<cr>", desc = "Force push" },
+    { "<Leader>gl", "<cmd> Git log --oneline --graph -n 40<cr>", desc = "Log" },
   },
 }
