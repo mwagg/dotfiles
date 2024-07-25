dd1641b87a6fd72ad0ac64906973627632924693
Mike Wagg <mike.wagg@shopify.com>
2024-07-25 16:29:57 +0100

    Fix vim-test which-key prefix

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -7,17 +7,10 @@ return {
     "vim-test/vim-test",
     cmd = { "TestNearest", "TestFile", "TestLast" },
     keys = {
+      { "<leader>t", "", desc = "+test" },
       { prefix .. "t", "<cmd>wall<cr><cmd>TestNearest<cr>", desc = "Run nearest test" },
       { prefix .. "f", "<cmd>wall<cr><cmd>TestFile<cr>", desc = "Run tests in current file" },
       { prefix .. "l", "<cmd>wall<cr><cmd>TestLast<cr>", desc = "Run last test" },
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      defaults = {
-        [prefix] = { name = "+test" },
-      },
     },
   },
 }
